6d1cd27d85d0dbf55f3e5dc66bcbef1fa7695dff
TarusheeAgarwal <109898224+TarusheeAgarwal@users.noreply.github.com>
2022-07-24 19:08:47 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,13 @@
-nes (8 sloc)  133 Bytes
-
 mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"21f1001231@student.onlinedegree.iitm.ac.in\"\n\
+" > ~/.streamlit/credentials.toml
+
 echo "\
 [server]\n\
 headless = true\n\
+enableCORS=false\n\
 port = $PORT\n\
-enableCORS = false\n\
-\n\
 " > ~/.streamlit/config.toml
